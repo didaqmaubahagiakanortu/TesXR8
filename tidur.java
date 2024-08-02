e78d8cd22c2c133e21529e71d6ab7a9d2ec799f4
didaq. <qurnaen_didaq_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:00:26 +0700

    Add files via upload

---
+++ tidur.java
@@ -0,0 +1,9 @@
+public class tidur {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int hasil = a + b;
+
+        System.out.println("Hasil dari " + a + " ditambah " + b + " adalah " + hasil + ".");
+    }
+}
